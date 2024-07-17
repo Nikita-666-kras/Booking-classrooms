3eabb751403f59285958a687f6bb381409daf3df
Nikita-666-kras <116032886+Nikita-666-kras@users.noreply.github.com>
2024-07-17 15:02:58 +0300

    com

---
+++ Backend/Booking-Classroom/src/main/java/org/example/bookingclassroom/DAL/DataAccessLayer.java
@@ -0,0 +1,15 @@
+package org.example.bookingclassroom.DAL;
+import org.example.bookingclassroom.Models.*;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import lombok.Getter;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+@Getter
+public class DataAccessLayer {
+}
