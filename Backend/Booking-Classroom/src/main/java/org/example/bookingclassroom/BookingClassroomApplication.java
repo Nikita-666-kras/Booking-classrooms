e6091adef438a983076e14f58eb49af95cf824cc
Nikita-666-kras <116032886+Nikita-666-kras@users.noreply.github.com>
2024-07-17 14:28:48 +0300

    /START

---
+++ Backend/Booking-Classroom/src/main/java/org/example/bookingclassroom/BookingClassroomApplication.java
@@ -0,0 +1,13 @@
+package org.example.bookingclassroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingClassroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingClassroomApplication.class, args);
+	}
+
+}
---
+++ Backend/Booking-Classroom/src/test/java/org/example/bookingclassroom/BookingClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookingclassroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
